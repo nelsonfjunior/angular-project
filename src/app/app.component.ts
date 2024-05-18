30c1b9a006f39e2e73d1d56d932e08cbb26f8f64
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-05-18 11:45:27 -0300

    ngOnDestroy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-project';
+
+  exibeHome: boolean = true;
+  destruir(){
+    this.exibeHome = false;
+  }
 }
