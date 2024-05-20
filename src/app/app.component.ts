4cb8a35808599abc0c84e608dd2f43e7ef76da11
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-05-20 10:28:52 -0300

    ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-project';
-
-  exibeHome: boolean = true;
-  destruir(){
-    this.exibeHome = false;
-  }
+  
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,20 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnDestroy{
-  ngOnDestroy(): void {
-    console.log('HomeComponent destroyed');
+export class HomeComponent implements DoCheck{
+
+  num: number = 0;
+
+  ngDoCheck(): void {
+    console.log('Uma mudanca foi feita')
+  }
+
+  adiciona1() {
+    this.num++;
   }
 
 }
