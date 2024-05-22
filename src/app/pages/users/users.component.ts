8190941cb146857b3d1514a1f7412011dda1ac32
nelsonfjunior <ferreiranelson123@hotmail.com>
2024-05-22 15:35:40 -0300

    reactiveForms including users

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from '../../models/user';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit{
   //users: String[] = ['Vinicius', 'Julio', 'Lucas', 'Rafael', 'Gabriel'];
   userSelecionado: User | undefined;
+  userForm: FormGroup = new FormGroup({});
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.userForm = this.fb.group({
+      nome:['', [Validators.required, Validators.maxLength(250)]],
+      idade:['', [Validators.required, Validators.min(12), Validators.max(110)]],
+    })
+  }
+
+  submitForm(){
+    if(this.userForm.valid){
+      this.users.push(this.userForm.value);
+      this.userForm.reset();
+    }
+  }
+
   users: User[] = [
     {
       nome: 'Vinicius',
